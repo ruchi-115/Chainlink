a775b454f997ed02dd5f49cb3450fc7ed7f0f1a6
ruchi-115 <ruchitasonawale115@gmail.com>
2023-10-17 01:47:55 +0530

    Final commit

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -23,7 +23,7 @@ contract TodoList {
   );
 
   constructor() public {
-    createTask("Check out dappuniversity.com");
+    createTask("Check Ethereum Wallet");
   }
 
   function createTask(string memory _content) public {
